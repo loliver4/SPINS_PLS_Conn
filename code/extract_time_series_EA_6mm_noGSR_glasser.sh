48e9a0dff1ef8db466e845bdf4f1f39b7ec65547
Lindsay Oliver <lindsaydoliv@gmail.com>
2021-04-06 14:58:08 -0400

    Updated path and names for EA residual activation files.

--- code/extract_time_series_EA_6mm_noGSR_glasser.sh
+++ code/extract_time_series_EA_6mm_noGSR_glasser.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --job-name EA_time_series
 #SBATCH --cpus-per-task=4
-#SBATCH --output=/scratch/loliver/SPINS_PLS_Conn/EA_time_series.txt
-#SBATCH --error=/scratch/loliver/SPINS_PLS_Conn/EA_time_series.err
+#SBATCH --output=/scratch/loliver/SPINS_PLS_Conn/EA_time_series_glasser.txt
+#SBATCH --error=/scratch/loliver/SPINS_PLS_Conn/EA_time_series_glasser.err
 
 module load ciftify
 
@@ -15,10 +15,12 @@ sub_dir=$(ls -d -- /scratch/loliver/SPINS_PLS_Conn/sub*) # lists ea subject dire
 
 for dir in ${sub_dir}; do
     subj=$(basename ${dir})
-    in_dir="/projects/ttan/SPINS/analysis/GLM_test/${subj}/6_mm_no_GSR"
+    in_dir="/projects/ttan/SPINS/analysis/GLM/${subj}/6_mm_no_GSR"
 
-  if [ ! -f $dir/$subj_EA_6mm_noGSR_glasser_meants.csv ]; then
-   ciftify_meants --outputcsv ${dir}/${subj}_EA_6mm_noGSR_glasser_meants.csv ${in_dir}/${subj}_glm_ea_1stlevel_residual.dscalar.nii /projects/loliver/SPINS_PLS_Conn/data/Q1-Q6_RelatedValidation210.CorticalAreas_dil_Final_Final_Areas_Group_Colors.32k_fs_LR.dlabel.nii
+  if [ ! -f $dir/$subj_EA_6mm_noGSR_glasser_meants_up.csv ]; then
+   ciftify_meants --outputcsv ${dir}/${subj}_EA_6mm_noGSR_glasser_meants_up.csv ${in_dir}/${subj}_task-emp_parametric-load_residual_contrast-map.dscalar.nii /projects/loliver/SPINS_PLS_Conn/data/Q1-Q6_RelatedValidation210.CorticalAreas_dil_Final_Final_Areas_Group_Colors.32k_fs_LR.dlabel.nii
   fi
 
 done
+
+
